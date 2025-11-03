88404e025246993efe4746bedc978b51303ed8c2
Dylan <dylan.blakemore@gmail.com>
2025-11-03 10:43:46 +0200

    Release version 1.0.9

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Depscheck.MixProject do
   def project do
     [
       app: :depscheck,
-      version: "1.0.8",
+      version: "1.0.9",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -70,7 +70,7 @@ defmodule Depscheck.MixProject do
         "LICENSE_COMPATIBILITY_RULES.md",
         "CHANGELOG.md"
       ],
-      source_ref: "v1.0.8"
+      source_ref: "v1.0.9"
     ]
   end
 
